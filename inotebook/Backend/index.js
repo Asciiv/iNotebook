32014b5005bc74d8727e68515edac871b7714472
kartikey <kartikeym1@gmail.com>
2023-06-12 22:29:43 +0530

    day-3

--- inotebook/Backend/index.js
+++ inotebook/Backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 
 
-app.use('/api/auth',require('./routes/auth.js'));
+app.use('/api/auth/',require('./routes/auth.js'));
 
 app.use('/api/notes',require('./routes/notes.js',));
 
--- inotebook/Backend/routes/auth.js
+++ inotebook/Backend/routes/auth.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const {body, validationResult} = require('express-validator');
 //import { Router } from 'express';
 
 // Create a user using: POST "/api/auth/" . Doesn't require Auth
 
-router.post('/',(req,res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:5}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be at least 5 characters').isLength({min:5}),
+
+],(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user))  //--> Promises returning
+    .catch(err => {console.log(err)
+        res.json({errors: 'Please enter a unique email address'})
+    });``
 })
 
 module.exports = router;
